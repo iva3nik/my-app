9e20868dad46d88bac3f8eda87a6adedf5ccb445
Ivan Nikitin <iva3nik@yandex.ru>
2022-04-18 15:21:53 +0300

    feat: add social icons

--- src/layout/components/Footer/Footer.tsx
+++ src/layout/components/Footer/Footer.tsx
@@ -1,31 +1,42 @@
 import React from 'react'
 import cn from 'classnames'
 
+import instagram from '../../../assets/icons/instagram_icon.png'
+import github from '../../../assets/icons/github_icon.png'
+import linkedin from '../../../assets/icons/linkedin_icon.png'
+
 import s from './Footer.module.scss'
 
-const socialLinks = [
-    { name: 'Github', link: 'https://github.com/iva3nik' },
-    { name: 'Linkedid', link: 'https://www.linkedin.com/in/ivan-nikitin-71b038230/' },
-    { name: 'Instagram', link: 'https://www.instagram.com/iva3nik/' },
+export interface TSocialLinks {
+  name: string
+  link: string
+  icon: string
+}
+
+const socialLinks: Array<TSocialLinks> = [
+  { name: 'Github', link: 'https://github.com/iva3nik', icon: github },
+  { name: 'Linkedid', link: 'https://www.linkedin.com/in/ivan-nikitin-71b038230/', icon: linkedin },
+  { name: 'Instagram', link: 'https://www.instagram.com/iva3nik/', icon: instagram },
 ]
 
 const Footer: React.FC = () => {
-    return (
-        <div className={cn(s.footer, 'main-container')}>
-            <div className={s.footer__container}>
-                <p className={s.footer__copyrights}>&copy; 2022 - Иван Никитин</p>
-                <ul className={s.footer__links}>
-                    {socialLinks.map((item, index) => (
-                        <li key={item.name + index} className={s.footer__link}>
-                            <a href={item.link} target='_blank' rel='noopener noreferrer'>
-                                {item.name}
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    )
+  return (
+    <div className={cn(s.footer, 'main-container')}>
+      <div className={s.footer__container}>
+        <p className={s.footer__copyrights}>&copy; 2022 - Иван Никитин</p>
+        <ul className={s.footer__links}>
+          {socialLinks.map((item, index) => (
+            <li key={item.name + index} className={s.footer__link}>
+              <a href={item.link} target='_blank' rel='noopener noreferrer'>
+                <img src={item.icon} alt={item.name} className={s.footer__icon} />
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
--- src/layout/components/Header/components/Navigation/Navigation.tsx
+++ src/layout/components/Header/components/Navigation/Navigation.tsx
@@ -4,36 +4,39 @@ import cn from 'classnames'
 
 import s from './Navigation.module.scss'
 
-const navLinks = [
-    { to: '/', title: 'About me' },
-    { to: '/works', title: 'Works' },
-    { to: '/experience', title: 'Experience' },
-    { to: '/places', title: 'Places' },
-    { to: '/contacts', title: 'Contacts' },
+export interface TNavLinks {
+  to: string
+  title: string
+}
+
+const navLinks: Array<TNavLinks> = [
+  { to: '/', title: 'About me' },
+  { to: '/works', title: 'Works' },
+  { to: '/experience', title: 'Experience' },
+  { to: '/places', title: 'Places' },
+  { to: '/contacts', title: 'Contacts' },
 ]
 
 const Navigation: React.FC = () => {
-    return (
-        <nav className={s.navigation}>
-            <ul className={s.navigation__menu}>
-                {navLinks &&
-                    navLinks.map((link, index) => (
-                        <li key={link.title + index}>
-                            <NavLink
-                                to={link.to}
-                                className={(navData) =>
-                                    navData.isActive
-                                        ? cn(s.navigation__link, s.navigation__link_active)
-                                        : s.navigation__link
-                                }
-                            >
-                                {link.title}
-                            </NavLink>
-                        </li>
-                    ))}
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className={s.navigation}>
+      <ul className={s.navigation__menu}>
+        {navLinks &&
+          navLinks.map((link, index) => (
+            <li key={link.title + index}>
+              <NavLink
+                to={link.to}
+                className={(navData) =>
+                  navData.isActive ? cn(s.navigation__link, s.navigation__link_active) : s.navigation__link
+                }
+              >
+                {link.title}
+              </NavLink>
+            </li>
+          ))}
+      </ul>
+    </nav>
+  )
 }
 
 export default Navigation
