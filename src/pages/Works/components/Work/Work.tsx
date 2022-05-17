8af65e59cea91022e0cca1ea0c61e872e9b666a8
Ivan Nikitin <iva3nik@yandex.ru>
2022-05-17 17:39:45 +0300

    feat: change styles works

--- src/pages/Works/components/Work/Work.tsx
+++ src/pages/Works/components/Work/Work.tsx
@@ -21,12 +21,18 @@ const Work = (props: TItem) => {
   return (
     <>
       {isShowedInfo ? (
-        <section className={cn(s.work, s.work_back)}>
+        <section className={cn(s.work, s.work_back, s.work_border)}>
           <div className={s.work__content}>
             <div className={s.work__text}>
               <p>Название проекта: {props.item.name}</p>
               <p>Описание проекта: {props.item.description}</p>
               <p>Стэк проекта: {props.item.stack}</p>
+              <a href={props.item.linkGithub} target='_blank'>
+                Переход к проекту на Github
+              </a>
+              <a href={props.item.linkGP} target='_blank'>
+                Переход к проекту на Github Pages
+              </a>
             </div>
             <Button onClick={handleButton} type={'naked'} fullWidth={false} customClassName={s.work__button}>
               Назад
@@ -36,7 +42,6 @@ const Work = (props: TItem) => {
       ) : (
         <section className={s.work} style={{ backgroundImage: `url(${props.item.photo})` }}>
           <div className={s.work__content}>
-            <h2>{props.item.name}</h2>
             <Button onClick={handleButton} type={'primary'} fullWidth={false} customClassName={s.work__button}>
               Подробнее
             </Button>
