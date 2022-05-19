751b0791980cb647a3c9058676c145e223bcc51e
Ivan Nikitin <iva3nik@yandex.ru>
2022-05-20 00:07:37 +0300

    fix: delete unnecessary lines

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import Layout from './layout/Layout'
-import Works from './pages/Works/Works'
-import NotFound from './pages/NotFound/NotFound'
 
 import { appRoutes } from './router'
 
