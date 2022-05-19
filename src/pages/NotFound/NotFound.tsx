c7da8e0e822bc162345ed8c87d195b16bdb294e5
Ivan Nikitin <iva3nik@yandex.ru>
2022-05-19 23:26:34 +0300

    feat: add picture for 404

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import page_not_found from '../../assets/images/NotFound/page_not_found.png'
+
 import s from './NotFound.module.scss'
 
 const NotFound: React.FC = () => {
   return (
     <section className={s.notfound}>
-      <h1>Page not found</h1>
+      <img className={s.notfound__image} src={page_not_found} alt='Page not found' />
     </section>
   )
 }
