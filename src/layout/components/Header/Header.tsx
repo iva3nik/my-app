2fb1d3b413229d002f9ce468fc4fed654d2ad574
Ivan Nikitin <iva3nik@yandex.ru>
2022-05-20 20:09:55 +0300

    feat: add adaptive for Header

--- src/layout/components/Header/Header.tsx
+++ src/layout/components/Header/Header.tsx
@@ -8,12 +8,14 @@ import logo from '../../../assets/svg/Ivan_Nikitin.svg'
 import s from './Header.module.scss'
 
 const Header: React.FC = () => {
-    return (
-        <header className={cn(s.header, 'main-container')}>
-            <img src={logo} alt='Ivan Nikitin' className={s.header__logo} />
-            <Navigation />
-        </header>
-    )
+  return (
+    <header className={cn(s.header, 'main-container')}>
+      <div className={s.header__container}>
+        <img src={logo} alt='Ivan Nikitin' className={s.header__logo} />
+        <Navigation />
+      </div>
+    </header>
+  )
 }
 
 export default Header
