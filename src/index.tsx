a2d82bb703c8c6d55d0ea2db12b881643dd692d8
Ivan Nikitin <iva3nik@yandex.ru>
2022-05-23 19:06:02 +0300

    fix: change BroueserRouter => HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
@@ -9,9 +9,9 @@ import './style/scss/index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 )
 
 reportWebVitals()
